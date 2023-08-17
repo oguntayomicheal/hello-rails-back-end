2f1f9282d184bf9b10c608040cd977745eff527e
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-18 00:01:48 +0100

    Fix linters errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 module Api
-    module V1
-      class GreetingsController < ApplicationController
-        def index
-          @greeting = Greeting.all.order('RANDOM()').limit(1)
-          render json: @greeting
-        end
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        @greeting = Greeting.all.order('RANDOM()').limit(1)
+        render json: @greeting
       end
     end
   end
-  
+end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,7 +14,7 @@ module HelloRailsBackEnd
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+        resource '*', headers: :any, methods: %i[get post put patch delete options head]
       end
     end
     # Initialize configuration defaults for originally generated Rails version.
